d518697c8152940b52dd151ce97a579ba0ca6557
goreleaserbot <goreleaser@carlosbecker.com>
2019-09-01 00:36:23 +0900

    Brew formula update for fsrpl version v0.0.5

--- fsrpl.rb
+++ fsrpl.rb
@@ -2,22 +2,21 @@
 class Fsrpl < Formula
   desc "fsrpl is CloudFirestore replication tool."
   homepage "https://github.com/matsu0228/fsrpl"
-  version "0.0.4"
+  version "0.0.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/matsu0228/fsrpl/releases/download/v0.0.4/fsrpl_0.0.4_darwin_amd64.tar.gz"
-    sha256 "ee989c554973a4f3cefe26813ca35165c2a93e8c0794c0da23b1fd46adfff7ec"
+    url "https://github.com/matsu0228/fsrpl/releases/download/v0.0.5/fsrpl_0.0.5_darwin_amd64.tar.gz"
+    sha256 "34c53042edd88dc1850ad77acf7a459718a1b302bd03af8d0bbfe00c36cd4768"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/matsu0228/fsrpl/releases/download/v0.0.4/fsrpl_0.0.4_linux_amd64.tar.gz"
-      sha256 "2d701ca87c618b9d44426f138db5afd953fb05bf1be4e5314f89e9498ce4a25a"
+      url "https://github.com/matsu0228/fsrpl/releases/download/v0.0.5/fsrpl_0.0.5_linux_amd64.tar.gz"
+      sha256 "e1d3f85223213e80e5c9473433635cb8cdec761062355acd708e61d251db3ed5"
     end
   end
 
   def install
-    bin.install Dir['fsrpl']
-    zsh_completion.install "misc/completions/zsh/_fsrpl"
+    bin.install "fsrpl"
   end
 
   test do
