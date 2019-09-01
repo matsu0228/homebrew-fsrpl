d750f29f6b1f8d41757344494e3498b6ea6c9626
goreleaserbot <goreleaser@carlosbecker.com>
2019-09-01 19:03:50 +0900

    Brew formula update for fsrpl version v0.1.0

--- fsrpl.rb
+++ fsrpl.rb
@@ -2,16 +2,16 @@
 class Fsrpl < Formula
   desc "fsrpl is CloudFirestore replication tool."
   homepage "https://github.com/matsu0228/fsrpl"
-  version "0.0.5"
+  version "0.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/matsu0228/fsrpl/releases/download/v0.0.5/fsrpl_0.0.5_darwin_amd64.tar.gz"
-    sha256 "34c53042edd88dc1850ad77acf7a459718a1b302bd03af8d0bbfe00c36cd4768"
+    url "https://github.com/matsu0228/fsrpl/releases/download/v0.1.0/fsrpl_0.1.0_darwin_amd64.tar.gz"
+    sha256 "1434d53797dc5898ed04b9a2e5090e5ec6e284985591a56c08fbc37bbf09a1cc"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/matsu0228/fsrpl/releases/download/v0.0.5/fsrpl_0.0.5_linux_amd64.tar.gz"
-      sha256 "e1d3f85223213e80e5c9473433635cb8cdec761062355acd708e61d251db3ed5"
+      url "https://github.com/matsu0228/fsrpl/releases/download/v0.1.0/fsrpl_0.1.0_linux_amd64.tar.gz"
+      sha256 "13c34f62186a4bb98148a74948f2b843347322a880fec493c63cb490c6867d35"
     end
   end
 
